4e883ecbf6012c5a7ee83a3ab6c983df606774f3
Le Viet Long <vilagondk@gmail.com>
2020-07-05 21:11:39 +0700

    update

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -2,6 +2,6 @@ cp .env.staging .env
 rm -rf .git .gitignore .eslintignore .eslintrc .circleci node_modules src typings test
 rm .env.example .env.staging .editorconfig README.md
 
-rsync -avzP . root@159.65.15.71:/home/sanna-tour-backend
+rsync -avzP . ubuntu@3.23.87.196:/home/sanna-tour-backend
 
-ssh root@159.65.15.71 "cd /home/sanna-tour-backend && npm install && npm rebuild && pm2 restart sannatour-api && exit"
+ssh ubuntu@3.23.87.196 "cd /home/sanna-tour-backend && npm install && npm rebuild && pm2 restart sannatour-api && exit"
