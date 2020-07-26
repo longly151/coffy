b91e8747cf7b8eaab66fbf1d64bd3fbe09a328d9
Le Viet Long <vilagondk@gmail.com>
2020-07-26 11:37:41 +0700

    Update

--- src/database/seeds-staging/04-user.seed.ts
+++ src/database/seeds-staging/04-user.seed.ts
@@ -8,6 +8,7 @@ export default class CreateUsers implements Seeder {
   public async run(factory: Factory): Promise<void> {
     // Hash password
     const hashPass: string = await Bcrypt.hash('admin');
+    const hashPassOperator: string = await Bcrypt.hash('operator');
     await getConnection()
       .createQueryBuilder()
       .insert()
@@ -26,8 +27,24 @@ export default class CreateUsers implements Seeder {
           note: 'Nothing',
           status: 'ACTIVE',
           roleId: 1
+        },
+        {
+          fullName: 'Operator',
+          email: 'operator@gmail.com',
+          password: hashPassOperator,
+          phone: '0327571918',
+          avatar:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlUbAyS_643dq_B69jZAlPNW6_Xc7SLELY6SpRsc5OI2wHiiYG&usqp=CAU',
+          gender: 'MALE',
+          birthday: '2011-10-05T14:48:00.000Z',
+          bio: "I'm admin",
+          note: 'Nothing',
+          status: 'ACTIVE',
+          roleId: 2
         }
-      ])
+      ]
+      )
       .execute();
+    await factory(User)({ roles: ['INTERN'] }).createMany(10);
   }
 }
